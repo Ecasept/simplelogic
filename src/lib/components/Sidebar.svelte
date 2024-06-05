8505481f3d35530865172e9e13ca828f065babcc
Arbee4ever <arbee@arbeeco.de>
2024-06-05 20:23:49 +0200

    feat: collapsible sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-	import Component from '$lib/components/Component.svelte';
 	import Canvas from '$lib/components/Canvas.svelte';
 
 	export let canvas: Canvas;
 
+	let open = true;
+
 	function addCmp() {
-		canvas.addCmp("test")
+		canvas.addCmp('test');
+	}
+
+	function collapse() {
+		open = !open;
 	}
 </script>
 
-<div class="sidebarWrapper">
-	<button on:click={addCmp}>Add Component</button>
+<div class="sidebarWrapper" class:open={open}>
+	<button class="collapse" on:click={collapse}>Collapse Sidebar</button>
+	<div class="content">
+		<button on:click={addCmp}>Add Component</button>
+	</div>
 </div>
 
 <style lang="scss">
@@ -21,5 +29,18 @@
 		right: 0;
 		top: 0;
 		background-color: red;
+		transition: left .3s ease-in-out;
+		left: 80vw;
+		display: flex;
+
+		&:not(.open) {
+			left: 99vw;
+		}
+
+		.collapse {
+			bottom: 0;
+			height: 100%;
+			width: 1vw;
+		}
 	}
 </style>
