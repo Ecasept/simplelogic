d978d6104cd5604a3021d06334f131d4b65f4536
Ecasept <76942454+Ecasept@users.noreply.github.com>
2024-08-27 10:35:56 +0200

    tests: fix playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:5173",
+		baseURL: "http://localhost:8788",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -72,8 +72,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "npm run build && npm run preview && npx wrangler pages dev",
-		url: "http://127.0.0.1:5173",
+		command: "npx wrangler pages dev",
+		url: "http://127.0.0.1:8788",
 		reuseExistingServer: !process.env.CI,
 	},
 });
