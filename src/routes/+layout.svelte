8683d7dc0c0b52063cb59cfedebcc8dab02d804f
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-01-11 10:08:41 +0100

    refactor: use svelte 5 syntax for layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import "$lib/css/global.scss";
 	import "$lib/css/normalize.css";
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
 
-<slot/>
+{@render children?.()}
