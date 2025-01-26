512a2000539427642149ed487958e0c3647e32fd
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-01-26 18:50:37 +0100

    refactor(test): move some common.ts functions into the editor fixture and move it into a separate file

--- playwright/mobile/mobile.spec.ts
+++ playwright/mobile/mobile.spec.ts
@@ -1,52 +1,21 @@
-import { expect, Locator, Page } from "@playwright/test";
-import { expectPosToBe, Pointer, test } from "../common";
-
-async function addComponent(
-	page: Page,
-	pointer: Pointer,
-	type: string,
-	x: number,
-	y: number,
-) {
-	await page
-		.locator(".sidebarWrapper")
-		.getByText(type, { exact: true })
-		.click();
-	await pointer.tap(x, y);
-}
-
-function comp(page: Page) {
-	return page.locator(".component-body");
-}
-
-async function connect(pointer: Pointer, handle1: Locator, handle2: Locator) {
-	await pointer.downOn(handle1);
-	await pointer.moveTo(handle2);
-	await pointer.up();
-}
-
-async function drag(pointer: Pointer, locator: Locator, x: number, y: number) {
-	await pointer.downOn(locator);
-	await pointer.move(x, y);
-	await pointer.up();
-}
+import { expect } from "@playwright/test";
+import { expectPosToBe, test } from "../common";
 
 test.describe("Mobile", () => {
-	test("can edit", async ({ page, touchscreen, editor }) => {
-		const pointer = touchscreen.createPointer();
-		await addComponent(page, pointer, "AND", 100, 100);
-		await addComponent(page, pointer, "AND", 200, 200);
+	test("can edit", async ({ page, editorMobile: editor }) => {
+		await editor.addComponent("AND", 100, 100);
+		await editor.addComponent("AND", 200, 200);
 
-		await expectPosToBe(comp(page).first(), 100, 100);
-
-		await connect(
-			pointer,
+		await editor.connect(
 			editor.getHandle("AND", "out").first(),
 			editor.getHandle("AND", "in1").nth(1),
 		);
 
-		await drag(pointer, comp(page).first(), 100, 100);
+		await editor.drag(editor.comp().first(), 100, 100);
 
-		await expect(page.locator(".wire")).toHaveAttribute("d", "M181 141 L241 261");
+		await expect(page.locator(".wire")).toHaveAttribute(
+			"d",
+			"M181 141 L241 261",
+		);
 	});
 });
