17faaf1e00b034e76ce3063cebb0d958b91e856a
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-02-07 18:30:28 +0100

    feat: put toolbar at bottom on mobile

--- src/lib/components/editor/OnCanvas.svelte
+++ src/lib/components/editor/OnCanvas.svelte
@@ -8,7 +8,8 @@
 	{@render content()}
 </div>
 
-<style>
+<style lang="scss">
+	@use "src/lib/css/variables.scss" as *;
 	#container {
 		position: absolute;
 		top: 0;
@@ -17,5 +18,12 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	@media (max-width: $mobile-breakpoint) {
+		#container {
+			flex-direction: column-reverse;
+			bottom: 0;
+		}
 	}
 </style>
