52ad7db1e50b10b55510762c6a8f48aa754348de
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-02-23 16:39:37 +0100

    feat: add logo (preliminary)

--- src/lib/components/editor/overlay/Logo.svelte
+++ src/lib/components/editor/overlay/Logo.svelte
@@ -1,1 +1,21 @@
-<div class="logo-overlay">Logo</div>
+<div class="container">
+	<img src="/icon.svg" alt="Logo" height="24" />impleLogic
+</div>
+
+<style>
+	.container {
+		background-color: var(--surface-color);
+		color: var(--on-component-background-color); /*var(--on-surface-color);*/
+		border-radius: var(--default-border-radius);
+		border: 1px solid var(--surface-border-color);
+		box-shadow: 0px 0px 10px var(--shadow-color);
+
+		margin: 10px;
+		padding: 10px;
+		line-height: 0;
+
+		/* Center the image and text */
+		display: flex;
+		align-items: center;
+	}
+</style>
