226b485411ff21c2898e13e27dd5a1e6340dfc9b
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-06-17 11:27:17 +0200

    refactor: remove unused login and logout methods from AuthViewModel

--- src/lib/util/viewModels/authViewModel.ts
+++ src/lib/util/viewModels/authViewModel.ts
@@ -1,4 +1,3 @@
-import { API } from "../api";
 import { ViewModel } from "./viewModel";
 
 export type AuthUiState = {
@@ -36,33 +35,6 @@ export class AuthViewModel extends ViewModel<AuthUiState> {
 	toggleOpen() {
 		this._uiState.open = !this._uiState.open;
 		this.notifyAll();
-	}
-	async login() {
-		this._uiState.message = "Logging in...";
-		this.notifyAll();
-		const data = await API.login(this._uiState.passwordInputValue);
-		if (data.success) {
-			this._uiState.message = null;
-			this._uiState.loggedIn = true;
-			this._uiState.passwordInputValue = "";
-			this.notifyAll();
-		} else {
-			this._uiState.message = data.error;
-			this.notifyAll();
-		}
-	}
-	async logout() {
-		this._uiState.message = "Logging out...";
-		this.notifyAll();
-		const data = await API.logout();
-		if (data.success) {
-			this._uiState.message = null;
-			this._uiState.loggedIn = false;
-			this.notifyAll();
-		} else {
-			this._uiState.message = data.error;
-			this.notifyAll();
-		}
 	}
 }
 
