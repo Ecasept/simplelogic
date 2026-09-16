ab3abf0f1bb49580b99e4aa99fb677c867a6dfd4
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-09-16 14:29:05 +0200

    refactor: move presets to their own files

---
+++ src/lib/server/presets.test.ts
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+import { presets } from "./presets";
+import { circuits } from "../../../playwright/circuits";
+import { ZGraphData } from "$lib/util/types";
+
+it.each(Object.entries(presets))(
+	"validates production preset %s",
+	(_name, preset) => {
+		expect(ZGraphData.safeParse(JSON.parse(preset.data)).error).toBeUndefined();
+	},
+);
+
+it.each(Object.entries(circuits))(
+	"validates test circuit %s",
+	(_name, data) => {
+		expect(ZGraphData.safeParse(JSON.parse(data)).error).toBeUndefined();
+	},
+);
