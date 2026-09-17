0bfc51e685c257756c60cdd40812eaf91fb66a75
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-09-17 19:55:08 +0200

    fix: add type specification to JSON imports in circuits.ts

--- playwright/circuits.ts
+++ playwright/circuits.ts
@@ -1,10 +1,10 @@
-import multiconnected from "./circuits/multiconnected.json";
-import singleAnd from "./circuits/singleAnd.json";
-import singleOR from "./circuits/singleOR.json";
-import rippleCarryAdder from "./circuits/rippleCarryAdder.json";
-import SR_NOR_latch from "./circuits/SR_NOR_latch.json";
-import wireTest from "./circuits/wireTest.json";
-import areaSelectTest from "./circuits/areaSelectTest.json";
+import multiconnected from "./circuits/multiconnected.json" with { type: "json" };
+import singleAnd from "./circuits/singleAnd.json" with { type: "json" };
+import singleOR from "./circuits/singleOR.json" with { type: "json" };
+import rippleCarryAdder from "./circuits/rippleCarryAdder.json" with { type: "json" };
+import SR_NOR_latch from "./circuits/SR_NOR_latch.json" with { type: "json" };
+import wireTest from "./circuits/wireTest.json" with { type: "json" };
+import areaSelectTest from "./circuits/areaSelectTest.json" with { type: "json" };
 
 export const circuits = {
 	multiconnected: JSON.stringify(multiconnected),
